b2b32eb996e810c7ef5e30c92f1b2167bea2600d
JNewton <jnnewton@hiwaay.net>
2021-02-07 21:39:25 -0600

    Created main file template

---
+++ main.py
@@ -0,0 +1,24 @@
+import math 
+import numpy as np
+
+class ProjectFailed(Exception):
+    '''Custom Project Exception that can be thrown for error analysis'''
+    pass
+
+
+
+
+
+
+def main() -> int:
+    return 1
+
+
+
+
+if __name__ == "__main__":
+    if main() != 1:
+        raise ProjectFailed("MAE 343 Project returned bad exit code!")
+
+
+
